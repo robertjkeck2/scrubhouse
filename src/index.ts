8f59468210a7a3709bd26cec39158d633baa8f32
John Keck <robertjkeck2@gmail.com>
2021-01-31 20:09:47 -0600

    basic format of express app

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,9 @@ const port: number = parseInt(process.env.PORT ?? '3000');
 
 app.use(express.json());
 
-app.get<Request>('/', (req, res) => {
+app.get<Request>('/', (_, res) => {
   console.log('Twitter OAuth');
+  res.sendStatus(200);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
